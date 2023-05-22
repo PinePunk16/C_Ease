0bafcf9be34657e0e39420a5e4755c24e485d669
PinePunk16 <106622968+PinePunk16@users.noreply.github.com>
2023-05-22 20:35:57 +0200

    Added comments

--- include/c_ease/loop.h
+++ include/c_ease/loop.h
@@ -3,6 +3,7 @@
 #ifndef LOOP_H
     #define LOOP_H
 
+    // Inverted start and end don't work yet
     #define for_range(index,start,end)      for(long long index = start; ((start < end)? index < end : index > end); index += ((start < end)? 1 : index -1))
 
 #endif
